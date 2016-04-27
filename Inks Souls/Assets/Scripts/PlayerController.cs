647be28bd2c801f12c89078120dfc6a3c328f16b
BrazilGabriel <gabriel.br1994@gmail.com>
2016-04-27 11:36:21 -0400

    novas funcionalidades e obstaculos
    adicionado:
    lasers, moedas com geração procedural;
    agora o player morre;
    score;
    reinicio da fase;

--- Inks Souls/Assets/Scripts/PlayerController.cs
+++ Inks Souls/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,74 @@ public class PlayerController : MonoBehaviour {
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
 
-    void FixedUpdate()
-    {
+    private bool dead = false;
+
+    private uint coins = 0;
+
+    public Texture2D coinIconTexture;
+
+    public GUIStyle restartButtonStyle;
+
+    void FixedUpdate() {
         bool jetpackActive = Input.GetButton("Fire1");
 
-        if (jetpackActive)
-        {
+        jetpackActive = jetpackActive && !dead;
+
+        if (jetpackActive) {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
         }
 
-        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
-        newVelocity.x = forwardMovementSpeed;
-        GetComponent<Rigidbody2D>().velocity = newVelocity;
+        if (!dead) {
+            Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+            newVelocity.x = forwardMovementSpeed;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+    }
 
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.CompareTag("Coins"))
+            CollectCoin(collider);
+        else
+            HitByLaser(collider);
+    }
+
+    void HitByLaser(Collider2D laserCollider) {
+        dead = true;
+    }
+
+    void CollectCoin(Collider2D coinCollider) {
+        coins++;
+
+        Destroy(coinCollider.gameObject);
+    }
+
+    void DisplayCoinsCount() {
+        Rect coinIconRect = new Rect(10, 10, 32, 32);
+        GUI.DrawTexture(coinIconRect, coinIconTexture);
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 30;
+        style.fontStyle = FontStyle.Bold;
+        style.normal.textColor = Color.yellow;
+
+        Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
+        GUI.Label(labelRect, coins.ToString(), style);
+    }
+
+    void OnGUI() {
+        DisplayCoinsCount();
+        DisplayRestartButton();
+
+    }
+
+    void DisplayRestartButton() {
+        if (dead /*&& grounded*/)
+        {
+            Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
+            if (GUI.Button(buttonRect, "Tap to restart!"))
+            {
+                Application.LoadLevel(Application.loadedLevelName);
+            };
+        }
     }
 }
