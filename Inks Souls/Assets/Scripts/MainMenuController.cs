77b68ed7f441573c5b1ad7598197b6f92b1b5871
Thiago Figueira <tsantosfigueira@outlook.com>
2016-04-28 13:21:45 -0300

    Menu [In progress]
    Adicionado botão "Play" que redireciona para o jogo;
    Adicionado botão "Exit" que permite ao jogador sair do jogo;
    Adicionado menu para confirmação da saída (no Unity, ao apertar-se "Yes"
    nada ocorre, conforme esperado);

---
+++ Inks Souls/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    public Canvas exitCanvas;
+    public Button playButton;
+    public Button exitButton;
+
+    void Awake()
+    {
+        exitCanvas.enabled = false;
+    }
+
+    // Pega os componentes necessários; Evita erros caso algum componente errado tenha sido passado;
+    void start()
+    {
+        exitCanvas = exitCanvas.GetComponent<Canvas>();
+        playButton = playButton.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+    }
+
+    //Carrega a primeira fase
+	public void playGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // O botão sair foi pressionado
+    public void exitGame()
+    {
+        exitCanvas.enabled = true;
+        playButton.enabled = false;
+        exitButton.enabled = false;
+    }
+
+    // Na confirmação de saída, o botão "Não" foi pressionado
+    public void noPressed()
+    {
+        exitCanvas.enabled = false;
+        playButton.enabled = true;
+        exitButton.enabled = true;
+    }
+
+    // Na confirmação de saída, o botão "Sim" foi pressionado
+    public void yesPressed()
+    {
+        Application.Quit();
+    }
+}
