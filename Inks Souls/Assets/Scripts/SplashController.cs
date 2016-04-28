cb7e8e8f9736af855e3381dce501fe67028717d0
Thiago Figueira <tsantosfigueira@outlook.com>
2016-04-28 15:30:46 -0300

    Tela de Splash
    A tela de splash foi introduzida. Aguarda-se 03 segundos para que ela
    desapareça.

---
+++ Inks Souls/Assets/Scripts/SplashController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (splashScreen());
+	}
+	
+	IEnumerator splashScreen(){
+		yield return new WaitForSeconds (3);
+		SceneManager.LoadScene ("StartMenu");
+	}
+}
